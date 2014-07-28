89b385dde78bdce311a6d0df09e47461297f2b8c
Chintan Choksi <chintan@chintans-mbp.poly.edu>
2014-07-28 16:17:03 -0400

    commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+setup(
+  name = 'affix',
+  packages = [''], 
+  version = '0.1',
+  description = "Seattle's affix-repo package",
+  author = 'Seattle',
+  url = 'https://github.com/BuildSeash/affix', # use the URL to the github repo
+  classifiers = [],
+)
